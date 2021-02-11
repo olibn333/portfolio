440bc052da8634f2c1b92286107de6fb4cda2252
oliverbn <olibn333@gmail.com>
2021-02-11 21:38:01 +0700

    hello init

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+
+const Header = () => {
+    return (
+        <Typography>Hello</Typography>
+    )
+}
+
+export default Header
---
+++ header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+
+const Header = () => {
+    return (
+        <Typography>Hello</Typography>
+    )
+}
+
+
+export default Header
+
